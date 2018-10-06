08664ea23941daab8f24c3de66f7751f41826919
Michael Karneim <mkarneim@googlemail.com>
2018-10-06 14:09:27 +0200

    Fix give-rocket.lua not working from command-block

--- give-rocket.lua
+++ give-rocket.lua
@@ -4,15 +4,21 @@
 
 local pkg = {}
 local module = ...
+local USAGE = "/give-rocket [<player>] [<count>]"
+local showError
+local tell
 
 function pkg.register()
   Commands.register("give-rocket",string.format([[
     require('%s').giveRocket(...)
-  ]],module), "/give-rocket [<player>] [<count>]", 1)
+  ]],module), USAGE, 1)
 end
 
 function pkg.giveRocket(arg1,arg2)
-  local player = spell.owner.name
+  local player = nil
+  if spell.owner then
+    player = spell.owner.name
+  end
   local num = 1
   if tonumber(arg1) then
     num = tonumber(arg1)
@@ -22,10 +28,28 @@ function pkg.giveRocket(arg1,arg2)
       num = tonumber(arg2)
     end
   end
-
+  if not player then
+    showError("Usage: "..USAGE)
+    return
+  end
   spell:execute([[/give %s minecraft:arrow %s 0 
     {Wand:"rocket-launcher",display:{Name:"Rocket Launcher",Lore:["Look at your target and swing this wand"]},ench:[{id:99,lvl:1}]}
   ]], player, num)
+end
+
+function showError(message, ...)
+  local n = select('#', ...)
+  if n>0 then
+    message = string.format(message, ...)
+  end
+  tell(spell.owner.name, message, 'red')
+end
+
+function tell(to, message, color)
+  color = color or 'white'
+  spell:execute([[
+    /tellraw %s [{"text":"%s","color":"%s"}]
+  ]], to, message, color)
 end
 
 return pkg
